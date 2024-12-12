a669451c6a4710a809f2477aa5d04b14c6b3a632
Henry <74014339+Henry-42@users.noreply.github.com>
2024-12-12 13:16:58 -0500

    P1 done

--- Day5.py
+++ Day5.py
@@ -7,7 +7,25 @@ def get_file_data(file_name):
 
 
 def get_middle_number(list2):
-    return list2[round(len(list2) / 2)]
+    print(list2[round(len(list2) // 2)])
+    return list2[round(len(list2) // 2)]
+
+
+def rule_followed():
+    pos = 0
+    pos2 = 0
+    for z in range(len(listed_numbers)):
+        for m in range(len(updates[i])):
+            if updates[i][m] == listed_numbers[z][0]:
+                pos = m
+
+            if updates[i][m] == listed_numbers[z][1]:
+                pos2 = m
+
+        if pos > pos2:
+            return False
+    return True
+
 
 
 file_data = get_file_data("Day5Input.txt")
@@ -21,30 +39,45 @@ for i in range(len(file_data)):  # Use in if you want to
     else:
         updates.append(file_data[i].split(","))
 
-print(rules)
+# print(rules)
 updates.pop(0)  # Account for the space delimiter
 print(updates)
 
 count = 0
 
 for i in range(len(updates)):
-    tor = True
     listed_numbers = []
     for x in range(len(rules)):
-        if rules[x][0] and rules[x][1] in updates[i]:
-            temp_list = (rules[x][0], rules[x][1])
-            listed_numbers.append(temp_list)
-    for z in range(len(listed_numbers)):
-            pos = 0
-            pos2 = 0
-            if updates[i] == listed_numbers[z][0]:
-                pos = i
-            if updates[i] == listed_numbers[z][1]:
-                pos2 = i
-            if pos > pos2:
-                tor = False
+        if rules[x][0] in updates[i]:
+            if rules[x][1] in updates[i]:
+                temp_list = (rules[x][0], rules[x][1])
+                listed_numbers.append(temp_list)
 
-    if tor:
+    torf = rule_followed()
+
+    if torf:
         count += int(get_middle_number(updates[i]))
 
 print(count)
+
+
+# Part 2
+
+list_of_incorrect = []
+
+for i in range(len(updates)):
+    
+    listed_numbers = []
+    for x in range(len(rules)):
+        if rules[x][0] in updates[i]:
+            if rules[x][1] in updates[i]:
+                temp_list = (rules[x][0], rules[x][1])
+                listed_numbers.append(temp_list)
+
+    torf = rule_followed()
+
+    if not torf:
+        list_of_incorrect.append(updates[i])
+
+print(list_of_incorrect)
+
