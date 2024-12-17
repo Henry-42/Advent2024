cb79fef64ebd11d1d94efe3173e10fa0eafb0f18
Henry <74014339+Henry-42@users.noreply.github.com>
2024-12-17 13:19:17 -0500

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -6,19 +6,23 @@ def get_file_data(file_name):
     return data
 
 
-file_data = get_file_data("Day6Input.txt")
+file_data = get_file_data("Day7Input.txt")
 print(file_data)
 
 
 def get_answer(array):
     return array.split(":")[0]
 
+# def get_index():
+
+# def get_torf:
+#     return
+
 
 def get_values(array):
     list_of_values = []
     arrayed = array.split(" ")
     arrayed.pop(0)
-    # print(arrayed)
     for m in range(len(arrayed)):
         arrayed[m] = int(arrayed[m])
 
@@ -28,9 +32,16 @@ def get_values(array):
 
 count = 0
 
+operators = ["*","+"]
+
 for i in range(len(file_data)):
-    print(get_answer(file_data[i]))
-    print(get_values(file_data[i]))
+    index = 0
+    answer = (get_answer(file_data[i]))
+    values = (get_values(file_data[i]))
     combination_index = len(get_values(file_data[i])) - 1
+    print(combination_index)
 
     for k in range(2**combination_index):
+        for g in range(combination_index - 1):
+            
+print(count)
